4b72839732250678ac504178316b9fb9ccab7a43
Pablo Merissi <pablo.merissi@hotmail.com>
2019-09-20 11:16:07 -0300

    Adição da tabuada

---
+++ exercicios/tabuada/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            Console.WriteLine("Tabuada do 5!");
+            for (i = 5; i <= 50; i+=5){
+            Console.WriteLine(i + " ");
+            }
+        }
+    }
+}
