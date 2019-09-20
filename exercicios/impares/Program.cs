ee3368e0984cfe334e6882c960343336e165bcd0
Pablo Merissi <pablo.merissi@hotmail.com>
2019-09-20 11:06:01 -0300

    Segundo commit

---
+++ exercicios/impares/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace impares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+
+         while (a <= 100)
+            {
+
+             if (a % 2 != 0)
+                {
+               Console.WriteLine("\n {0}", a);   
+                }
+
+                 a++; 
+            }
+        }
+    }
+}
+
+//for (int i= 1; i <= 100; i+2){
+//    Console.WriteLine(i + " ");
+//}
---
+++ exercicios/par/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace par
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n;
+            int fn;
+            do{
+            Console.WriteLine("Numeros ìmpares e pares");
+            Console.WriteLine("\nDigite um número: ");
+            n = double.Parse(Console.ReadLine());
+
+            if( n % 2 ==0)
+            {
+                Console.WriteLine("O seu número é par");
+                Console.WriteLine("\nO seu número é: {0}", n);
+            }
+            else{
+                Console.WriteLine("O seu número é ímpar");
+                Console.WriteLine("\nO seu número é: {0}", n);
+            }
+             Console.WriteLine("Aperte 1 para jogar novamente ou 2 Para sair");
+			 fn = int.Parse(Console.ReadLine()); 		
+			}while(fn==1);
+            
+        }
+    }
+}
