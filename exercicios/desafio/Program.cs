41cb3ef18a38cdfd969ae6f4b65ea59e6632f506
Pablo Merissi <pablo.merissi@hotmail.com>
2019-09-25 11:30:21 -0300

    vetor e matriz

---
+++ exercicios/desafio/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace desafio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                     int n, c, l, y;
+            Console.WriteLine ("Digite a quantida de triangulos: ");
+            y = int.Parse (Console.ReadLine ());
+            Console.WriteLine ("Digite o Tamanho: ");
+            n = int.Parse (Console.ReadLine ());
+
+            for (int q = 1; q <= y; q++) {
+                for (l = 1; l <= n; l++) {
+                    for (c = 1; c <= l; c++)
+                        Console.Write ("*");
+                    Console.Write ("\n");
+                }
+            }
+        }
+        }
+    }
+
---
+++ exercicios/matriz/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace matriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int iconpar = 0, isoma=0, iconipar= 0, isomai=0;
+            int [] number;
+            number = new int[6]; 
+            number[0] = 1;
+            number[1] = 2;
+            number[2] = 3;
+            number[3] = 4;
+            number[4] = 5;
+            number[5] = 6;
+
+             for(int i = 0; i < 6; i++)
+            {
+                if((number[i]%2)==0){
+                    iconpar++;
+                    isoma += number[i];
+                    
+                     Console.WriteLine(number[i].ToString());
+                }
+                if((number[i]%2)==1){
+                    iconipar++;
+                    isomai += number[i];
+                     Console.WriteLine(number[i].ToString());
+                }
+                
+            }
+           
+           
+            Console.WriteLine("A soma dos numeros pares é: {0}",isoma); 
+            Console.WriteLine("A quantidade de Numeros pares é: {0}",iconpar);
+            
+            
+            
+            Console.WriteLine("\nA soma dos numeros ímpares e: {0}",isomai); 
+            Console.WriteLine("A quantidade de numeros ímpares e: {0}",iconipar);
+            
+
+        }
+    }
+}
