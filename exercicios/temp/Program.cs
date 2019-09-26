3a4ad205e3484b3319923580ee68f4365114e8b7
Pablo Merissi <pablo.merissi@hotmail.com>
2019-09-26 11:26:15 -0300

    Matriz continuacao

--- exercicios/temp/Program.cs
+++ exercicios/temp/Program.cs
@@ -6,40 +6,32 @@ namespace temp
     {
         static void Main(string[] args)
         {
-           int iconpar = 0, isoma=0, iconipar= 0, isomai=0;
-            int [] number;
-            number = new int[6]; 
-            number[0] = 1;
-            number[1] = 2;
-            number[2] = 3;
-            number[3] = 4;
-            number[4] = 5;
-            number[5] = 6;
 
-             for(int i = 0; i < 6; i++)
-            {
-                if((number[i]%2)==0){
-                    iconpar++;
-                    isoma += number[i];
-                    
-                     Console.WriteLine(number[i].ToString());
-                }
-                if((number[i]%2)==1){
-                    iconipar++;
-                    isomai += number[i];
-                     Console.WriteLine(number[i].ToString());
-                }
-                
-            }
-           
-           
-            Console.WriteLine("A soma dos numeros pares é: {0}",isoma); 
-            Console.WriteLine("A quantidade de Numeros pares é: {0}",iconpar);
-            
-            
-            
-            Console.WriteLine("\nA soma dos numeros ímpares e: {0}",isomai); 
-            Console.WriteLine("A quantidade de numeros ímpares e: {0}",iconipar);
+           double [] temperaturas = new double [12];
+           double maior = 0, menor = 0;
+
+           for(int i = 0; i < 12; i++)
+           {
+               Console.WriteLine("Digite a temparatura do mês: ");
+               temperaturas[i] = double.Parse(Console.ReadLine());
+           }
+
+           maior = temperaturas[0];
+           menor = temperaturas[0];
+
+           foreach(double temp in temperaturas)
+           {
+               if(temp > maior)
+               {
+                   maior  = temp;
+               }
+               else if (temp < menor){
+                   menor = temp;
+               }
+           }
+
+            Console.WriteLine("A temperatura maior é: {0}", maior);
+            Console.WriteLine("A temperatura menor é: {0}", menor);
         }
     }
 }
