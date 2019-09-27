c3feaa6533853e10f26cc92cf5c005650486682b
Pablo Merissi <pablo.merissi@hotmail.com>
2019-09-27 11:18:54 -0300

    Classe, instancial

---
+++ mcbonalds/mcbonalds/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace mcbonalds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente cliente1 = new Cliente("Pablo","55 11 2020-2020", "pbo@merissi.com");
+            Console.WriteLine("Nome: " + cliente1.Nome);
+            Console.WriteLine("Telefone: " + cliente1.Telefone);
+            Console.WriteLine("Email: " + cliente1.Email);
+        }
+    }
+}
